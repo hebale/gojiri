947ff7da45634776afdefa4bd9b350715df71bfa
hebale <hebale@hankookilbo.com>
2023-03-22 18:30:06 +0900

    add vue-Splide library

--- src/layout/GojiriHeader.vue
+++ src/layout/GojiriHeader.vue
@@ -1,22 +1,83 @@
+<script>
+  import { onMounted, ref } from 'vue';
+  import { Splide, SplideSlide } from '@splidejs/vue-splide';  
+  import { getMonth, getYear } from 'date-fns';
+
+
+  
+
+  export default {
+    components: {
+      Splide,     
+      SplideSlide
+    },    
+    data() {
+      return {
+        SplideOptions: {
+          type: 'loop',
+          arrows: false, // shadow button
+          pagination: false,
+          lazyLoad: false,
+          drag: 'free',
+          snap: true,
+          perPage: 3,
+          focus: 'center',
+        }
+      }
+    },
+    setup() {
+      const today = ref({ 
+        year: getYear(new Date),
+        month: getMonth(new Date)
+      });
+
+      const splide = ref();
+
+      onMounted(() => {
+        if (splide.value && splide.value.splide) {
+          const proxySplide = splide.value.splide;
+          const control = document.createDocumentFragment();
+
+          ['prev', 'next'].forEach((el) => {
+            const dom = document.createElement('div');
+
+            dom.classList.add(el);
+            dom.addEventListener('click', () => {
+              proxySplide.go( el === 'prev' ? '-1' : '+1');
+            });
+            control.appendChild(dom);
+          })
+          
+          proxySplide.root.querySelector('.splide__track').appendChild(control);
+
+
+          
+          proxySplide.mount({ Intersection });
+        }
+      });
+
+
+      const test = (entry) => {
+        console.log(entry)
+      }
+
+
+      return {
+        today,
+        splide,        
+        test
+      };
+    },
+  }
+</script>
+
 <template>
   <header>
-    <button type="button">이전</button>
-    <ul>
-      <li>
-        <a href="/">1월</a>
-        <a href="/">2월</a>
-        <a href="/">3월</a>
-        <a href="/">4월</a>
-        <a href="/">5월</a>
-      </li>
-    </ul>
-    <button type="button">다음</button>
+    <p class="year">{{ today.year }}</p>
+    <Splide ref="splide" @splide:intersection="test" :options="SplideOptions" >
+      <SplideSlide v-for="(value, index) in 12" :key="`slide_${ index }`" >
+        <Button type="button">{{ value }}</Button>
+      </SplideSlide>
+    </Splide>
   </header>
 </template>
-
-<script>
-export default {
-  setup(){
-  }
-}
-</script>
