7dd61ea7c3394e8167b7ba5288c9a3e417a249d4
hebale <hebale@hankookilbo.com>
2023-03-24 03:12:08 +0900

    motion added

--- src/layout/Go-Header.vue
+++ src/layout/Go-Header.vue
@@ -4,11 +4,9 @@
   import Flicking from '@egjs/vue3-flicking';
   import { Perspective } from '@egjs/flicking-plugins';
 
-
   export default {
     components: {
-      Flicking,     
-      // SplideSlide
+      Flicking
     },    
     data() {
       return {
@@ -24,27 +22,33 @@
           autoResize: true,
           preventEventsBeforeInit: true,
         },
-        plugins: [
-          new Perspective({ 
-            seletor: 'active',
-            scale: 0.8,
-            rotate: 0,
-          })
-        ]
+        // plugins: [
+        //   new Perspective({ 
+        //     seletor: 'active',
+        //     scale: 0.8,
+        //     rotate: 0,
+        //   })
+        // ]
       }
     },
     methods: {       
-      move(event) {        
+      slideMove(event) {        
         event.currentTarget.panels.forEach(panel => {
-          -0.2 < panel.progress && panel.progress < 0.2
-            ? panel.element.classList.add('active')
-            : panel.element.classList.remove('active')
+          const progress = Math.abs(panel.progress.toFixed(2));
+
+          if (progress < 1.5) {
+            panel.element.style.transform = `scale(${Math.max(0.7, 1 - (progress / 2.1))})`
+          } else {
+            panel.element.style.transform = `scale(0.7)`
+          }
+
+          if (progress < 0.2) {
+            panel.element.classList.add('active')
+          } else {
+            panel.element.classList.remove('active')
+          }
         })
       },
-      // moveEnd(event) {
-
-        
-      // },
       slideToPrev() {
         this.$refs.flicking.prev();
       },
@@ -63,26 +67,8 @@
 
       onMounted(() => {
         if (flicking.value && flicking.value.flicking) {
-        //   const proxySplide = splide.value.splide;
-        //   const control = document.createDocumentFragment();
-
-        //   ['prev', 'next'].forEach((el) => {
-        //     const dom = document.createElement('div');
-
-        //     dom.classList.add(el);
-        //     dom.addEventListener('click', () => {
-        //       proxySplide.go( el === 'prev' ? '-1' : '+1');
-        //     });
-            
-        //     control.appendChild(dom);
-        //   })
-          
           console.log(flicking.value.flicking)
-        //   proxySplide.root.querySelector('.splide__track').appendChild(control);
         }
-
-
-
       });
 
       return {
@@ -102,36 +88,15 @@
       :cameraTag="'ul'"
       :plugins="plugins"
 
-      @move="move"
+      @move="slideMove"
     >
       <li v-for="(value, index) in 12" :key="`slide_${ index }`" >
         <button type="button">{{ value }}</button>
       </li>
-
       <template #viewport>
-        <button
-          type="button"
-          class="prev"
-          @click="slideToPrev"
-        >
-          이전
-        </button>
-        <button
-          type="button"
-          class="next"
-          @click="slideToNext"
-        >
-          다음
-        </button>
+        <button type="button" class="prev" @click="slideToPrev">이전</button>
+        <button type="button" class="next" @click="slideToNext">다음</button>
       </template>
     </Flicking>
-
-
-
-    <!-- <Splide ref="splide" @splide:dragging="event" :options="SplideOptions" >
-      <SplideSlide v-for="(value, index) in 12" :key="`slide_${ index }`" >
-        <Button type="button">{{ value }}</Button>
-      </SplideSlide>
-    </Splide> -->
   </header>
 </template>
