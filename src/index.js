b812af820464805ad163ec07b08a2c2b9c58d4b3
hebale <hebaleman@gmail.com>
2023-06-16 09:52:25 +0900

    스타일 height 수정

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import flicking from '@egjs/vue3-flicking'
 
 const app = createApp(App)
 
+const setVh = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+window.addEventListener('resize', setVh);
+setVh();
+
 app.use(store)
 app.use(flicking)
 app.mount('#app')
